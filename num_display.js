91ee3a9cc19eec59b2d667d4aa37935354df26d2
JJH <skkuwjswnsgur456@gmail.com>
2021-08-18 16:46:10 +0900

    cal end

--- num_display.js
+++ num_display.js
@@ -1,4 +1,5 @@
 let space=document.getElementById('cal-space');
+//숫자 버튼 
 const num_one = document.getElementById('one-button');
 const num_two = document.getElementById('two-button');
 const num_three = document.getElementById('three-button');
@@ -9,16 +10,22 @@ const num_seven = document.getElementById('seven-button');
 const num_eight = document.getElementById('eight-button');
 const num_nine = document.getElementById('nine-button');
 const num_zero = document.getElementById('zero-button');
+// 계산 기호
 const plus_button =document.getElementById('plus-button');
 const minus_button =document.getElementById('minus-button');
 const divide_button =document.getElementById('divide-button');
 const multiple_button =document.getElementById('multiple-button');
 const equal_button = document.getElementById('equal-button');
+const reverse_button = document.getElementById('reverse-button');
+const percent_button = document.getElementById('percent-button');
+// 중간 과정 
 let calReady=false;
 let FIRST_NUM=0;
 let SECOND_NUM=0;
 let CAL;
 let RESULT=0;
+// all clear
+const allClearButton = document.getElementById('all-clear');
 
 function handleNumClick(event){
     if (space.innerText=='0'){
@@ -38,9 +45,20 @@ function handleEqualClick(event) {
     SECOND_NUM=space.innerText;
     space.innerText=eval(FIRST_NUM+CAL+SECOND_NUM);
 }
+function allClear(){
+    FIRST_NUM=0;
+    SECOND_NUM=0;
+    CAL=NaN;
+    space.innerText='0';
+}
 
+function handleReverse(){
+    space.innerText=eval('-1*'+space.innerText);
+}
 
-
+function percent(){
+    space.innerText=eval('0.01*'+space.innerText);
+}
 
 
 num_one.addEventListener('click',handleNumClick);
@@ -59,3 +77,7 @@ minus_button.addEventListener("click",handleCal);
 divide_button.addEventListener("click",handleCal);
 multiple_button.addEventListener("click",handleCal);
 equal_button.addEventListener("click",handleEqualClick);
+reverse_button.addEventListener('click',handleReverse);
+percent_button.addEventListener('click',percent);
+
+allClearButton.addEventListener('click',allClear);
